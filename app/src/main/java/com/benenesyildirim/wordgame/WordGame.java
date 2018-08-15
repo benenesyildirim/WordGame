4bb232aa4ac0f66193e4b955d9a53e5e401a9d2e
benenesyildirim <benenesyildirim@gmail.com>
2018-08-15 16:56:14 +0300

    It's First Open Of Code

---
+++ app/src/main/java/com/benenesyildirim/wordgame/WordGame.java
@@ -0,0 +1,13 @@
+package com.benenesyildirim.wordgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WordGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_word_game);
+    }
+}
